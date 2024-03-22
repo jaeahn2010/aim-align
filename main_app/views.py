bf4a9a5bf192e8e362f5c13398930b8a882242c7
jaeahn2010 <jaeahn2010@gmail.com>
2024-03-22 11:58:02 -0700

    set up skeleton code for home, about, and goals index pages

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
+# mock data
+goals = [
+    {
+        'id': 1,
+        'title': 'finish project',
+        'start_date': '2024-03-22',
+        'end_date': '2024-03-29',
+    },
+    {
+        'id': 2,
+        'title': 'finish housework',
+        'start_date': '2024-03-22',
+        'end_date': '2024-03-24',
+    },
+]
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def goals_index(request):
+    return render(request, 'goals/index.html', {
+        'goals': goals
+    })
